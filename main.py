5ad8f624a9be4656ccf368782c841ec955e01827
Fonthom <thomasf0n7a1n3@gmail.com>
2026-05-10 19:47:55 +0200

    added --verbose option

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 def main():
     load_dotenv()
@@ -19,13 +22,16 @@ def main():
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents= args.user_prompt
+        contents= messages
     )
-    try:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    except RuntimeError:
-        print("Token usage information is not available for this response.")
+
+    if args.verbose:
+        try:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        except RuntimeError:
+            print("Token usage information is not available for this response.")
 
     print(response.text)
 
